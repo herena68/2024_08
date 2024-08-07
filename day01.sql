7548edb20c2863c1b0b145c45ed16909210f7e58
herena68 <kkjr7@daum.net>
2024-08-07 21:03:52 +0900

    Add files via upload

---
+++ day01.sql
@@ -0,0 +1,24 @@
+
+use 한빛무역;
+SELECT * FROM 고객;
+SELECT * FROM 마일리지등급;
+SELECT * FROM 부서;
+SELECT * FROM 사원;
+SELECT * FROM 제품;
+SELECT * FROM 주문;
+SELECT * FROM 주문세부;
+SELECT * FROM 한빛무역.고객;
+
+use scott;
+SELECT * FROM emp;
+SELECT * FROM dept;
+SELECT * FROM scott.emp;
+
+-- mysql -u root -p
+-- mysql -u user01 -p
+
+show databases;
+show tables;
+
+desc 고객;
+
